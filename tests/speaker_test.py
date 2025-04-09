49afd501df4a399e304b3adc5036fce9d3ba69ad
AntonySilverhand <3327652328@qq.com>
2025-04-09 11:58:02 +0800

    Remove speech_structure import and simplify speaker test functions

--- tests/speaker_test.py
+++ tests/speaker_test.py
@@ -16,17 +16,8 @@ speaker_with_prompt = [
 
 
 
-class Speaker(Debater):
-    def __init__(self, api_key, motion, position):
-        super().__init__(api_key, motion)
-        self.speaker_tone = speaker_tone
-        
-    def announce_motion(self, async_client: AsyncOpenAI) -> None:
-        # TODO: make it more natural and use llm to generate the texts here.
-        text = "Ladies and gentlemen, welcome to this debate. The motion reads: {motion}, now you have 1 minute to read the motion and then you will have 15 minutes for prep time.".format(motion=self.motion)
-        asyncio.run(tts(async_client=async_client, tone=self.speaker_tone, input=text))
+def announce_motion():
+    pass
 
-    def announce_next_speaker(self, async_client: AsyncOpenAI, current_speaker_position: str, next_speaker_position: str) -> None:
-        # TODO: make it more natural and use llm to generate the texts here.
-        text = "Thank you {} for that very fine speech, now let's welcome {} to deliver his speech, hear hear.".format(current_speaker_position, next_speaker_position)
-        asyncio.run(tts(async_client=async_client, tone=self.speaker_tone, input=text))
+def announce_next_speaker():
+    pass
