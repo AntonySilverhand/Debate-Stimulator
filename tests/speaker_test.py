908ca37fb9ad9d650bd5a503d75e51a2a3dd5ef6
AntonySilverhand <3327652328@qq.com>
2025-04-09 11:58:49 +0800

    Remove speech_structure import and add test function implementations

--- tests/speaker_test.py
+++ tests/speaker_test.py
@@ -1,14 +1,14 @@
-import speech_structure
+
 
 speaker_with_prompt = [
-    ["Prime Minister", [speech_structure.prime_minister_speech, debater_tone]],
-    ["Leader of Opposition", [speech_structure.leader_of_opposition_speech, debater_tone]],
-    ["Deputy Prime Minister", [speech_structure.deputy_prime_minister_speech, debater_tone]],
-    ["Deputy Leader of Opposition", [speech_structure.deputy_leader_of_opposition_speech, debater_tone]],
-    ["Member of Government", [speech_structure.member_of_government_speech, debater_tone]],
-    ["Member of Opposition", [speech_structure.member_of_opposition_speech, debater_tone]],
-    ["Government Whip", [speech_structure.government_whip_speech, debater_tone]],
-    ["Opposition Whip", [speech_structure.opposition_whip_speech, debater_tone]]
+    ["Prime Minister", ["speech_structure.prime_minister_speech, debater_tone"]],
+    ["Leader of Opposition", ["speech_structure.leader_of_opposition_speech, debater_tone"]],
+    ["Deputy Prime Minister", ["speech_structure.deputy_prime_minister_speech, debater_tone"]],
+    ["Deputy Leader of Opposition", ["speech_structure.deputy_leader_of_opposition_speech, debater_tone"]],
+    ["Member of Government", ["speech_structure.member_of_government_speech, debater_tone"]],
+    ["Member of Opposition", ["speech_structure.member_of_opposition_speech, debater_tone"]],
+    ["Government Whip", ["speech_structure.government_whip_speech, debater_tone"]],
+    ["Opposition Whip", ["speech_structure.opposition_whip_speech, debater_tone"]]
 ]
 
 
@@ -17,7 +17,16 @@ speaker_with_prompt = [
 
 
 def announce_motion():
-    pass
+    print("Motion announced")
 
-def announce_next_speaker():
-    pass
+def announce_next_speaker(current_position: str, next_position: str):
+    print("Next speaker: {}".format(next_position))
+
+def announce_end():
+    print("End of debate")
+
+
+if __name__ == "__main__":
+    announce_motion()
+    current_speaker = "Prime Minister"
+    announce_end()
