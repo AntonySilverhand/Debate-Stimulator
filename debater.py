90c83e76e00e83309f5172b01f80d61d3ba56932
AntonySilverhand <3327652328@qq.com>
2025-04-10 19:05:25 +0800

    Refactor debater class and move speaker test to tests directory

--- debater.py
+++ debater.py
@@ -4,8 +4,16 @@ import os
 from dotenv import load_dotenv
 import speech_structure
 import asyncio
+from text_generator import OpenAI_Responder
 
 load_dotenv()
+
+"""
+This file is to structure the final_prompt to be passed in the text generator.
+
+There should be one def prompt_loader() that uses a general way of combining the different prompts and return the final_prompt, position should be a variable here.
+
+"""
 
 debater_tone = os.getenv("debater_tone")
 
@@ -21,23 +29,35 @@ speaker_with_prompt = [
 ]
 
 
-def load_prompt(motion: str, position: str) -> str:
-    final_prompt = speaker_with_prompt[position][1][0] + "\n\n" + motion
+def prompt_loader(motion: str, position: str, speech_log: list) -> str:
+    """
+    This function is to structure the final_prompt to be passed in the text generator.
+    
+    Args:
+        motion (str): The motion of the debate.
+        position (str): The position of the speaker.
+        speech_log (list): The list of previous speakers' conversations.
+    
+    Returns:
+        str: The final_prompt to be passed in the text generator.
+
+    TODO: The Prime Minister has no previous speaker, this should be optimized.
+    """
+    final_prompt = speaker_with_prompt[position][1][0] + "\n\n" + "The motion reads: " + motion + "\n\n" + "The previous speakers conversation: " + speech_log
     return final_prompt
 
 class Debater:
-    def __init__(self, client: OpenAI, motion: str):
+    def __init__(self, client: OpenAI, motion: str, position: str, speech_log: list):
         self.client = client
         self.motion = motion
+        self.position = position
+        self.speech_log = speech_log
 
-    def respond_to(self, prompt: str) -> str:
-        response = self.client.chat.completions.create(
-            model="o1-mini",
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
-        )
-        return response.choices[0].message.content
+    def _respond_to(self, prompt: str) -> str:
+        responder = OpenAI_Responder(model="o1-mini", api_key=os.getenv("OPENAI_API_KEY"))
+        final_prompt = prompt_loader(self.motion, self.position, self.speech_log)
+        response = responder.respond_to(final_prompt)
+        return response
 
 
 
--- text_generator.py
+++ text_generator.py
@@ -9,6 +9,9 @@ This file is for generating text responses of the speaker's and the debaters'.
 There should be at least OpenAI and OpenRouter as providers.
 More is expected in the future.
 
+It is designed to respond to only one message, so system message and message history is not supported.
+The message should be passed as a string and be loaded with being passed in.
+
 Wether the response is used for Speaker or Debater should be defined in main.py instead of here.
 """
 
@@ -30,73 +33,6 @@ class OpenAI_Responder:
 if __name__ == "__main__":
     responder = OpenAI_Responder(model="o1-mini", api_key=os.getenv("OPENAI_API_KEY"))
     print(responder.respond_to("Hello, how are you?"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import os
-from openai import OpenAI
-
-"""
-This file is for generating text responses of the speaker's and the debaters'.
-There should be at least OpenAI and OpenRouter as providers.
-More is expected in the future.
-
-Wether the response is used for Speaker or Debater should be defined in main.py instead of here.
-"""
-
-class OpenAI_Responder:
-    def __init__(self, model: str, api_key: str):
-        self.client = OpenAI(api_key=api_key)
-
-    def respond_to(self, message: str) -> str:
-        response = self.client.chat.completions.create(
-            model=model,
-            messages=[
-                {"role": "user", "content": message}
-            ]
-        )
-        return response.choices[0].message.content
-
-
-if __name__ == "__main__":
-    responder = OpenAI_Responder(model="o1-mini", api_key=os.getenv("OPENAI_API_KEY"))
-    print(responder.respond_to("Hello, how are you?"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
